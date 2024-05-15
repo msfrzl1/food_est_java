c5ac1cdc44deb14e79ff26ec3f24c7eada5114e2
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-05-15 19:08:22 +0700

    use useRoutes in app.jsx and browser router in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import FormLogin from "./components/Fragments/FormLogin";
+import { useRoutes } from 'react-router-dom';
+import { RouteList } from './components/Routes/Route';
 
 function App() {
-   return (
-      <>
-         <FormLogin/>
-      </>
-   );
+   const element = useRoutes(RouteList);
+
+   return element;
 }
 
 export default App;
